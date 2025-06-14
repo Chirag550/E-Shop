ad07b6f0967361378e9fa1ee6ef011b9b99503f3
Chirag <chiragmunjal550@gmail.com>
2025-06-14 19:51:53 +0530

    Done with error handling and database/prisma setup and also model for users

---
+++ packages/error-handler/error-middleware.ts
@@ -0,0 +1,17 @@
+import { AppError } from ".";
+import { Response, Request } from "express";
+
+export const errorMiddleware = (err: Error, req: Request, res: Response) => {
+  if (err instanceof AppError) {
+    console.log(`Error ${req.method} ${req.url} - ${err.message}`);
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: "err.message",
+      ...(err.details && { detals: err.details }),
+    });
+  }
+  console.log("unhandles error", err);
+  return res.status(500).json({
+    error: "Something went wrong , please try again!",
+  });
+};
